1fa77202fe03ffae9e03b5dbce8f28081a5bf39b
Jean <JFBimCopinski@gmail.com>
2024-10-28 08:59:38 -0300

    feat: config

---
+++ src/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+(async () => {
+  try {
+    await mongoose.connect(process.env.URI);
+    console.log("Conexão com o banco efetuada com sucesso.");
+  } catch (err) {
+    console.log(err);
+  }
+})();

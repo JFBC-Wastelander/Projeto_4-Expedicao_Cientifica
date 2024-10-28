e32844b42bd612a6e9c1f66ea8af03eaab54c76c
Jean <JFBimCopinski@gmail.com>
2024-10-28 09:11:05 -0300

    feat: finished && not tested code

---
+++ src/routers/especie_router.js
@@ -0,0 +1,19 @@
+import { Router } from "express"
+
+import {
+    store,
+    index,
+    show,
+    update,
+    destroy
+} from "../controllers/especie_controller.js"
+
+const router = Router()
+
+router.post("/", store)
+router.get("/", index)
+router.get("/:id", show)
+router.put("/:id", update)
+router.destroy("/:id", destroy)
+
+export default router
